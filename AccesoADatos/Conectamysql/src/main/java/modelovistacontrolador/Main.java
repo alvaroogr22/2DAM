21d696e2b6084b2769fce6d0ee9223b87bb5f8be
Álvaro Gómez Rueda <alvaroogr22@gmail.com>
2023-11-17 10:42:45 +0100

    vista controlador

---
+++ AccesoADatos/Conectamysql/src/main/java/modelovistacontrolador/Main.java
@@ -0,0 +1,19 @@
+package modelovistacontrolador;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String dbURL = "jdbc:mysql://localhost:17770/simulacro";// URL de la base de datos
+        String dbUser = "root"; // Usuario de la base de datos
+        String dbPassword = ""; // Contraseña del usuario
+
+        try {
+            ClienteController controller = new ClienteController(dbURL, dbUser, dbPassword); // Crea una instancia del controlador
+            controller.mostrarClientes(); // Llama al método del controlador para mostrar los clientes
+            controller.cerrarConexion(); // Llama al método del controlador para cerrar la conexión a la base de datos
+        } catch (SQLException e) {
+            e.printStackTrace(); // Maneja las excepciones de SQL imprimiendo el error
+        }
+    }
+}
