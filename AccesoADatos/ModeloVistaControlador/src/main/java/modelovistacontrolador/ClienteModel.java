56facd47ab107ff28b10d232d2745ba4ada156fe
Álvaro Gómez Rueda <alvaroogr22@gmail.com>
2023-12-14 18:29:26 +0100

    VC

--- AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/ClienteModel.java
+++ AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/ClienteModel.java
@@ -1,16 +1,14 @@
 package modelovistacontrolador;
-<<<<<<<< HEAD:AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/ClienteModel.java
-========
-
->>>>>>>> b5b0ff9d8ceaf5475dd92794131a5e3a5436b50f:AccesoADatos/Examen1AGomez/src/main/java/modelovistacontrolador/ClienteModel.java
 import java.util.List;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteModel {
     private Connection conn;
@@ -21,25 +19,21 @@ public class ClienteModel {
         conn = DriverManager.getConnection(dbURL, dbUser, dbPassword);
     }
 
-    // Método para obtener todos los clientes de la base de datos y devolverlos como
-    // una lista
+    // Método para obtener todos los clientes de la base de datos y devolverlos como una lista
     public List<Cliente> getAllClientes() throws SQLException {
         List<Cliente> clientes = new ArrayList<>(); // Inicializa una lista para almacenar los clientes
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM tvideojuegos"); // Crea una declaración SQL
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM clientes"); // Crea una declaración SQL
 
-        // Ejecuta una consulta SQL para obtener todos los clientes de la tabla
-        // "clientes"
+        // Ejecuta una consulta SQL para obtener todos los clientes de la tabla "clientes"
         ResultSet rs = stmt.executeQuery();
 
         // Itera a través de los resultados y crea objetos Cliente para cada fila
         while (rs.next()) {
             int id = rs.getInt("id");
+            String nif = rs.getString("nif");
             String nombre = rs.getString("nombre");
-            int anio = rs.getInt("anio");
-            String compañia = rs.getString("compañía");
-            Float precio = rs.getFloat("precio");
-            String sinopsis = rs.getString("sinopsis");
-            clientes.add(new Cliente(id, nombre, anio, compañia, precio, sinopsis)); // Agrega cada Cliente a la lista
+            String edad = rs.getString("edad");
+            clientes.add(new Cliente(id, nif, nombre, edad)); // Agrega cada Cliente a la lista
         }
 
         stmt.close(); // Cierra la declaración
@@ -52,3 +46,4 @@ public class ClienteModel {
         conn.close();
     }
 }
+
--- AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/Main.java
+++ AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/Main.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/Main.java
 package modelovistacontrolador;
-========
-package modelovistacontroladorphoneland;
->>>>>>>> b5b0ff9d8ceaf5475dd92794131a5e3a5436b50f:AccesoADatos/Conectamysql/src/main/java/modelovistacontroladorphoneland/Main.java
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<<< HEAD:AccesoADatos/ModeloVistaControlador/src/main/java/modelovistacontrolador/Main.java
         String dbURL = "jdbc:mysql://localhost:33306/empresa";// URL de la base de datos
-========
-        String dbURL = "jdbc:mysql://localhost:33306/phoneland";// URL de la base de datos
->>>>>>>> b5b0ff9d8ceaf5475dd92794131a5e3a5436b50f:AccesoADatos/Conectamysql/src/main/java/modelovistacontroladorphoneland/Main.java
         String dbUser = "root"; // Usuario de la base de datos
         String dbPassword = ""; // Contraseña del usuario
 
